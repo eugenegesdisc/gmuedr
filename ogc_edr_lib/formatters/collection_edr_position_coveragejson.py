e722e70a5e4b38094b2be94980e112bb1dbc7898
Eugene Genong Yu <gyu@gmu.edu>
2022-10-24 08:10:49 -0400

    update20221024_3

---
+++ ogc_edr_lib/formatters/collection_edr_position_coveragejson.py
@@ -0,0 +1,46 @@
+import json
+from ogc_edr_lib.formatters.base import BaseFormatter
+
+
+class CollecionsPositionCoverageJsonFormatter(BaseFormatter):
+    """generic Formatter ABC"""
+    type = "formatter"
+    category = "collection_edr_position"
+    supported_media_types = ["application/prs.coverage+json"]
+
+    def __init__(self, formatter_def):
+        """
+        Initialize object
+
+        :param formatter_def: formatter definition
+
+        :returns: pygeoapi.formatter.base.BaseFormatter
+        """
+        super().__init__(formatter_def)
+        # self.type = 'formatter'
+
+        # self.mediatypes = formatter_def['mediatype']
+
+        # self.name = formatter_def['name']
+
+    def write(self, options={}, data=None):
+        """
+        Generate data in specified format
+
+        :param options: CSV formatting options
+        :param data: dict representation of GeoJSON object
+
+        :returns: string representation of format
+        """
+        if data is not None:
+            if isinstance(data, dict):
+                jsonstr = json.dumps(
+                    data, default=self._default_json_string_converter)
+            else:
+                jsonstr = data
+        else:
+            jsonstr = ""
+        return jsonstr
+
+    def __repr__(self):
+        return '<CollecionsJsonFormatter> {}'.format(self.name)
